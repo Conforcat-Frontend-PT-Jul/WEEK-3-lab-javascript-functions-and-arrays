23f50708e9a7ef32c9cd98cb44d5d0dfb16df8e8
claudiamurall <claudiamurall@gmail.com>
2022-08-13 17:06:47 +0200

    doing iteration 4

--- src/functions-and-arrays.js
+++ src/functions-and-arrays.js
@@ -34,8 +34,18 @@ function findLongestWord(words) {
 // Iteration #3: Calculate the sum
 const numbers = [6, 12, 1, 18, 13, 16, 2, 1, 8, 10];
 
-function sumNumbers() {}
+function sumNumbers(numbers) {
+  if (numbers.length === 0){
+  return null;
+}
+  let sum = 0;
+ 
+  for (let i = 0; i < numbers.length; i++) {
+    sum += numbers[i];
+}
 
+return sum;
+}
 
 
 // Iteration #3.1 Bonus:
@@ -47,8 +57,16 @@ function sum() {}
 // Level 1: Array of numbers
 const numbersAvg = [2, 6, 9, 10, 7, 4, 1, 9];
 
-function averageNumbers() {}
+function averageNumbers(numbersAvg) {
+  let sum = 0;
+  for (let i = 0; i < numbersAvg.length; i++) {
+    sum += numbersAvg[i];
+  }
+  let avg = sum / numbersAvg.length;
+  return avg;
+}
 
+// should return null if receives an empty array when called
 
 // Level 2: Array of strings
 const wordsArr = ['seat', 'correspond', 'linen', 'motif', 'hole', 'smell', 'smart', 'chaos', 'fuel', 'palace'];
