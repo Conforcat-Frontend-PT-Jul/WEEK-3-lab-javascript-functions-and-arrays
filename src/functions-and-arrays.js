6deee4b69e17fe8c2e761432f2df90d0bb60894b
Leandro Balaguer <leandro.balaguer@gmail.com>
2022-08-27 02:30:30 +0200

    Update functions-and-arrays.js

--- src/functions-and-arrays.js
+++ src/functions-and-arrays.js
@@ -1,48 +1,65 @@
+
 // Iteration #1: Find the maximum
-const nums = [1, 8];
-
-function maxOfTwoNumbers() {
-  console.log( Math.max.apply(null, (nums))); 
+function maxOfTwoNumbers(a, b){
+  if (a > b) {
+    return a;
+  } else if (a < b){
+return b;
+  }
+  else{
+    return a, b;
+  }
 }
-maxOfTwoNumbers();
-
+console.log(maxOfTwoNumbers(8, 43));
 
 
 // Iteration #2: Find longest word
 const words = ['mystery', 'brother', 'aviator', 'crocodile', 'pearl', 'orchard', 'crackpot'];
 
-function findTheLongestWord(){
-  let findTheLongestWord = '';
+function findLongestWord(){
+  let findLongestWord = "";
   for (let i of words) 
-  if (i.length > findTheLongestWord.length)findTheLongestWord = i
- console.log(findTheLongestWord);
+  if (i.length >= findLongestWord.length)findLongestWord = i
+ console.log(findLongestWord);
 }
-findTheLongestWord();
+findLongestWord();
 
 
 // Iteration #3: Calculate the sum
+// Iteration #3.1 Bonus:
 const numbers = [6, 12, 1, 18, 13, 16, 2, 1, 8, 10];
 
 function sumNumbers() {
-    let sumNumbers=0;
-    for(let i of numbers)sumNumbers+=i;
-    console.log(sumNumbers);
+  let sumNumbers=0;
+  for(let i of numbers)sumNumbers+=i;
+  console.log(sumNumbers/10);
 }
 
 sumNumbers();
 
 
-// Iteration #3.1 Bonus:
-function sum() {}
-
-
+/*
+// Bonus - Iteration #3.2: A generic sum() function
+const mixedArr = [6, 12, 'miami', 1, true, 'barca', '200', 'lisboa', 8, 10];
+function sum() {
+  
+}
+function sum()
+*/
 
 // Iteration #4: Calculate the average
+function sumNumbers() {
+  let sumNumbers=0;
+  for(let i of numbers)sumNumbers+=i;
+  console.log(sumNumbers/10);
+}
+sumNumbers();
+
 // Level 1: Array of numbers
 const numbersAvg = [2, 6, 9, 10, 7, 4, 1, 9];
 
-function averageNumbers() {}
-
+function averageNumbers() {
+}
 
 // Level 2: Array of strings
 const wordsArr = ['seat', 'correspond', 'linen', 'motif', 'hole', 'smell', 'smart', 'chaos', 'fuel', 'palace'];
